6ec464ded9fcc808172d2873cd6a55422e826615
yishaq-official <yishaq.official@gmail.com>
2026-03-09 17:48:35 +0300

    allowing requests from any where

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ tables.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Movie Night Agent API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
